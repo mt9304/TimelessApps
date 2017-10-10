d470df9353422fbb2d8f8870648594a52c2b3d97
MT S PC <mt9304@gmail.com>
2017-10-09 17:50:16 -0700

    Added template layout. Beginning to polish and edit.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -34,4 +34,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 		assert_response :success
 		assert_select "title", "Contact | #{@base_title}"
 	end
+
+	test "should get blankpage" do
+		get blankpage_path
+		assert_response :success
+		assert_select "title", "Blank Page | #{@base_title}"
+	end
 end
