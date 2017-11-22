4b6e8adb59b582504924a9e087519a015b1dfe89
MT9304 <mt9304@gmail.com>
2017-11-21 16:58:38 -0800

    Fixed search function to check for blank results.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ApplicationRecord
+	validates :project_name, uniqueness: true
 
 	def self.search(search)
-		if search && where('project_name' => "#{search}").first.to_s.split("").count > 0
+		#if search && where('project_name' => "#{search}").first.to_s.split("").count > 0
+		#Checks if project exists in database. elsif not found, gives not found. else display the default project template. 
+		if search && where('project_name' => "#{search}").first.present?
 			where('project_name' => "#{search}").first
+		elsif search && where('project_name' => "#{search}").first.blank?
+			where('project_name' => 'MT001').first
 		else
 			where('project_name' => 'Project ID').first
 		end
