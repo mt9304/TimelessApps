8828121262d4df25515ab322264f77ffbfda675b
Max <mt9304@gmail.com>
2018-08-12 23:15:22 -0700

    Added flash css.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,14 +13,24 @@ module ApplicationHelper
   def flash_messages(_opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert #{boostrap_class(msg_type.to_sym)} fade in") do
-        concat(content_tag(:button, id: "close-button", class: "close", type: :button, data: { dismiss: 'alert' }, "aria-label" => :Close) do
-          concat content_tag(:span, "&times;".html_safe, "aria-hidden" => true)
-        end)
+
         concat message
       end)
     end
     nil
   end
+
+  #def flash_messages(_opts = {})
+  #  flash.each do |msg_type, message|
+  #    concat(content_tag(:div, message, class: "alert #{boostrap_class(msg_type.to_sym)} fade in") do
+  #      concat(content_tag(:button, id: "close-button", class: "close", type: :button, data: { dismiss: 'alert' }, "aria-label" => :Close) do
+  #        concat content_tag(:span, "&times;".html_safe, "aria-hidden" => true)
+  #      end)
+  #      concat message
+  #    end)
+  #  end
+  #  nil
+  #end
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
